9676d498c634e76f2f4c2b33db1eb542e966f25f
iTwicKz <twickes32@ufl.edu>
2016-01-04 20:53:40 -0500

    Finished third video of CodePath Week 1

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Takashi Wickes on 1/4/16.
+//  Copyright © 2016 Takashi Wickes. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
